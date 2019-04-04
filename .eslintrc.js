ec562556d5311287b3a7ffd46d54b674944465cb
Charlie Guan <charliegdev@gmail.com>
2019-04-03 23:15:06 -0700

    :wrench: Add ESLint configs

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-app/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:jsx-a11y/recommended'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: ['react', 'react-app', 'import', 'jsx-a11y'],
+  rules: {}
+};
