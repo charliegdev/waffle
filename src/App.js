4cb48acba355f6ebd618388562336da0108e0bf1
Charlie Guan <charliegdev@gmail.com>
2019-04-13 17:57:08 -0700

    :lipstick: Style background

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import { DatePicker } from 'antd';
+import React from 'react';
 import { Task } from './components/task';
-import './App.css';
 import 'antd/dist/antd.css';
+import styles from './App.module.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <DatePicker />
-          <hr />
-          <Task title="A sample task">
-            <p>Description line 1</p>
-            <p>Description line 2</p>
-            <p>Description line 3</p>
-            <p>Description line 4</p>
-          </Task>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className={styles.container}>
+    <Task title="A sample task">
+      <p>Description line 1</p>
+      <p>Description line 2</p>
+      <p>Description line 3</p>
+      <p>Description line 4</p>
+    </Task>
+  </div>
+);
 
 export default App;
