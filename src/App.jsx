66a0cc73b1be86e6700cf583ba8c7d1bc48535f2
Charlie Guan <charliegdev@gmail.com>
2019-04-14 20:52:37 -0700

    :lipstick: Add some mock task lanes

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,29 @@ import styles from './App.module.css';
 const App = () => (
   <div className={styles.container}>
     <Topbar />
+
     <TaskLane laneTitle="To Do">
       {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+        <Task title="A sample task">
+          <p>Description line 1</p>
+          <p>Description line 2</p>
+          <p>Description line 3</p>
+        </Task>
+      ))}
+    </TaskLane>
+
+    <TaskLane laneTitle="In Progress">
+      {[1, 2, 3, 4, 5, 6, 7].map((index) => (
+        <Task title="A sample task">
+          <p>Description line 1</p>
+          <p>Description line 2</p>
+          <p>Description line 3</p>
+        </Task>
+      ))}
+    </TaskLane>
+
+    <TaskLane laneTitle="Ready for Test">
+      {[1, 2, 3].map((index) => (
         <Task title="A sample task">
           <p>Description line 1</p>
           <p>Description line 2</p>
