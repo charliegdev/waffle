1ff56a540deda462d02f741137ed98c58a34694f
Charlie Guan <charliegdev@gmail.com>
2019-04-19 19:09:17 -0700

    :recycle: Refactor data structure.

---
+++ src/constants.js
@@ -0,0 +1,8 @@
+const status = {
+  TO_DO: 'To Do',
+  IN_PROGRESS: 'In Progress',
+  READY_FOR_TEST: 'Ready for Test',
+  COMPLETED: 'COMPLETED'
+};
+
+export default status;
--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,37 +1,58 @@
 import React from 'react';
+import status from '../constants';
 import { Task, TaskLane } from '../components/task';
 
+const mockTasks = [
+  {
+    id: 1,
+    title: 'Review iPhone XS Max',
+    description: 'Review the newest flagship iPhone.',
+    status: status.TO_DO
+  },
+  {
+    id: 2,
+    title: 'Review iPhone XR',
+    description: 'Review the newest "budget-friendly" iPhone.',
+    status: status.TO_DO
+  },
+  {
+    id: 3,
+    title: 'Review Samsung Galaxy S10 Plus',
+    description: 'Review the newest Samsung flagship',
+    status: status.IN_PROGRESS
+  },
+  {
+    id: 4,
+    title: 'Review Google Pixel 3 XL',
+    description: "Can the Google's flagship compete with Apple and Samsung?",
+    status: status.IN_PROGRESS
+  },
+  {
+    id: 5,
+    title: 'Review OnePlus 6T',
+    description: 'How much are we giving up for a "flagship killer"?',
+    status: status.COMPLETED
+  },
+  {
+    id: 6,
+    title: 'Review BlackBerry KEY2',
+    description: 'Are physical keyboards still relevant, and worth the tradeoffs?',
+    status: status.COMPLETED
+  }
+];
+
 const Tasks = () => (
+  // Can't guarantee the order of Object.keys(status), so do it manually. Should find a better way
   <div>
-    <TaskLane laneTitle="To Do" index={0}>
-      {[1, 2, 3, 4, 5, 6, 7].map((index) => (
-        <Task title="A sample task">
-          <p>Description line 1</p>
-          <p>Description line 2</p>
-          <p>Description line 3</p>
-        </Task>
-      ))}
-    </TaskLane>
-
-    <TaskLane laneTitle="In Progress" index={1}>
-      {[1, 2, 3, 4, 5, 6, 7].map((index) => (
-        <Task title="A sample task">
-          <p>Description line 1</p>
-          <p>Description line 2</p>
-          <p>Description line 3</p>
-        </Task>
-      ))}
-    </TaskLane>
-
-    <TaskLane laneTitle="Ready for Test" index={2}>
-      {[1, 2, 3].map((index) => (
-        <Task title="A sample task">
-          <p>Description line 1</p>
-          <p>Description line 2</p>
-          <p>Description line 3</p>
-        </Task>
-      ))}
-    </TaskLane>
+    {[status.TO_DO, status.IN_PROGRESS, status.READY_FOR_TEST, status.COMPLETED].map((status, index) => (
+      <TaskLane laneTitle={status} index={index}>
+        {mockTasks
+          .filter((task) => task.status === status)
+          .map((task) => (
+            <Task title={task.title}>{task.description}</Task>
+          ))}
+      </TaskLane>
+    ))}
   </div>
 );
 
