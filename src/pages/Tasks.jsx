3cee46db73841ad6f32289304c5b6ad904b4bad3
Charlie Guan <charliegdev@gmail.com>
2019-05-24 16:26:56 -0700

    :sparkles: Feed data from Redux store.

--- src/pages/Tasks.jsx
+++ src/pages/Tasks.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import { mockTasks, status } from '../constants';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { status } from '../constants';
 import { Task, TaskLane } from '../components/task';
 
-const Tasks = () => (
+const Tasks = ({ tasks }) => (
   <div>
     {Object.entries(status).map(([key, stringLiteral], index) => (
       <TaskLane laneTitle={stringLiteral} index={index} key={stringLiteral}>
-        {mockTasks
+        {tasks
           .filter((task) => task.status === stringLiteral)
           .map((task) => (
             <Task title={task.title} key={task.title}>
@@ -18,4 +20,23 @@ const Tasks = () => (
   </div>
 );
 
-export default Tasks;
+Tasks.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      status: PropTypes.string.isRequired
+    })
+  )
+};
+
+Tasks.defaultProps = {
+  tasks: []
+};
+
+const mapState = ({ tasks }) => ({
+  tasks
+});
+
+export default connect(mapState)(Tasks);
