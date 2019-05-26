6c2fc4626801760a040f07fb140335f82c5bc20a
Charlie Guan <charliegdev@gmail.com>
2019-05-25 21:56:28 -0700

    :sparkles: Able to move a Task to another column.

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -10,20 +10,33 @@ const options = Object.entries(status).map(([key, { title }]) => ({
   value: title
 }));
 
-const Task = ({ status, title, children }) => (
+const Task = ({ changeStatus, task }) => (
   <Card interactive elevation={Elevation.TWO} className={styles.container}>
-    <H5 className={styles.title}>{title}</H5>
-    {children}
+    <H5 className={styles.title}>{task.title}</H5>
+    {task.description}
     <div className={styles.menuButton}>
-      <SelectMenu initial={status} options={options} onSelect={item => console.log(item)} />
+      <SelectMenu
+        initial={task.status}
+        options={options}
+        onSelect={newStatus =>
+          changeStatus({
+            ...task,
+            status: newStatus
+          })
+        }
+      />
     </div>
   </Card>
 );
 
 Task.propTypes = {
-  status: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  children: PropTypes.node.isRequired
+  changeStatus: PropTypes.func.isRequired,
+  task: PropTypes.shape({
+    description: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    status: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired
+  }).isRequired
 };
 
 export default Task;
