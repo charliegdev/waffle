d6d120e63b63543e791008835d3c1e083d6acd72
Charlie Guan <charliegdev@gmail.com>
2019-05-27 16:55:25 -0700

    :white_check_mark: Add render test and snapshot test to Dialog.

--- src/components/dialog/Dialog.test.jsx
+++ src/components/dialog/Dialog.test.jsx
@@ -1,5 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
+import { Button } from "evergreen-ui";
+import Dialog from "./Dialog";
+
 describe("Dialog", () => {
-  it("renders without crashing", () => {
-    console.log("OK");
+  let dialog;
+
+  beforeEach(() => {
+    dialog = (
+      <Dialog
+        confirmLabel="OK"
+        onConfirm={() => undefined}
+        title="Sample Dialog"
+        trigger={<Button>Open Dialog</Button>}
+      >
+        <p>Sample Dialog content</p>
+      </Dialog>
+    );
+  });
+
+  test("Dialog renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(dialog, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("Dialog renders the same snapshot", () => {
+    const component = renderer.create(dialog);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
