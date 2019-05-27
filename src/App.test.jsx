ed5f3046cef0dc731c135190c16fcb8b1be86762
Charlie Guan <charliegdev@gmail.com>
2019-05-27 16:39:22 -0700

    :white_check_mark: Add a new snapshot test for App.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,16 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import renderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import App from "./App";
 import { store } from "./redux";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
-    div
-  );
-  ReactDOM.unmountComponentAtNode(div);
+describe("App root", () => {
+  test("App renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(
+      <Provider store={store}>
+        <App />
+      </Provider>,
+      div
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test("App renders the same snapshot", () => {
+    const component = renderer.create(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
