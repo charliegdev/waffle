21713621979ba176213eb83452f73a1d10325834
Charlie Guan <charliegdev@gmail.com>
2019-05-28 22:38:22 -0700

    :sparkles: Use React.memo to reduce rerender

--- src/components/task/Task.jsx
+++ src/components/task/Task.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
+import debounce from "lodash/debounce";
 import { Button } from "evergreen-ui";
 import { Card, Elevation, H5 } from "@blueprintjs/core";
 import styles from "./Task.module.scss";
-import { SelectMenu } from "../select-menu";
-import { status } from "../../constants";
-
-const options = Object.entries(status).map(([key, { title }]) => ({
-  label: title,
-  value: title
-}));
 
 const enableDrag = event => {
   event.stopPropagation();
@@ -18,46 +12,43 @@ const enableDrag = event => {
   event.dataTransfer.setData("text/plain", "some_dummy_data");
 };
 
-const Task = ({ updateTask, deleteTask, task }) => (
-  <Card
-    draggable
-    onDrag={event => {
-      // Some elements have undesired default drag behaviour. Prevent that.
-      event.preventDefault();
-      updateTask({
-        ...task,
-        dragging: true
-      });
-    }}
-    onDragStart={enableDrag}
-    interactive
-    elevation={Elevation.TWO}
-    className={styles.container}
-  >
-    <H5 className={styles.title}>{task.title}</H5>
-    {task.description}
-    <div className={styles.buttons}>
-      <SelectMenu
-        initial={task.status}
-        options={options}
-        onSelect={newStatus =>
-          updateTask({
-            ...task,
-            status: newStatus
-          })
-        }
-      />
-      <Button className={styles.deleteButton} intent="danger" onClick={() => deleteTask(task.id)}>
-        Delete
-      </Button>
-    </div>
-  </Card>
-);
+const Task = ({ updateTask, deleteTask, task }) => {
+  // The 'drag' event is fired several times per second; don't want to call our redux functions at the same frequency.
+  const updateDebounced = debounce(() => updateTask({ ...task, dragging: true }), 500, {
+    leading: true,
+    trailing: false
+  });
+
+  return (
+    <Card
+      className={styles.container}
+      draggable
+      elevation={Elevation.TWO}
+      interactive
+      onDrag={event => {
+        // Some elements have undesired default drag behaviour. Prevent that.
+        event.preventDefault();
+        updateDebounced();
+      }}
+      onDragStart={enableDrag}
+    >
+      <H5 className={styles.title}>{task.title}</H5>
+      {task.description}
+      <div className={styles.buttons}>
+        <p>Status: {task.status}</p>
+        <Button className={styles.deleteButton} intent="danger" onClick={() => deleteTask(task.id)}>
+          Delete
+        </Button>
+      </div>
+    </Card>
+  );
+};
 
 Task.propTypes = {
   deleteTask: PropTypes.func.isRequired,
   task: PropTypes.shape({
     description: PropTypes.string.isRequired,
+    dragging: PropTypes.bool.isRequired,
     id: PropTypes.number.isRequired,
     status: PropTypes.string.isRequired,
     title: PropTypes.string.isRequired
@@ -65,4 +56,17 @@ Task.propTypes = {
   updateTask: PropTypes.func.isRequired
 };
 
-export default Task;
+/** @typedef { import('../../redux/modules/tasks').Task } TaskProps */
+
+/**
+ * a Task can have many props, but we only care about these 4 (e.g., we don't care about 'dragging')
+ * This doesn't scale very well when we change the implementation of Task in the future, but for now the readability gain
+ * is worth it.
+ * @param {{ task: TaskProps}} prevProps
+ * @param {{ task: TaskProps}} nextProps
+ * @returns {boolean}
+ */
+const isSameId = (prevProps, nextProps) =>
+  ["description", "id", "status", "title"].every(property => prevProps.task[property] === nextProps.task[property]);
+
+export default React.memo(Task, isSameId);
