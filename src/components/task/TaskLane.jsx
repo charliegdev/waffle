04909882c0dc97e2ca89843be93f08c4806f0520
Charlie Guan <charliegdev@gmail.com>
2019-05-29 13:25:34 -0700

    :fire: Remove the unnecessary optimization

--- src/components/task/TaskLane.jsx
+++ src/components/task/TaskLane.jsx
@@ -1,44 +1,32 @@
-import React, { useState } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import classNames from "classnames";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import { Icon } from "@blueprintjs/core";
 import styles from "./TaskLane.module.scss";
 import { actions } from "../../redux";
 
-const TaskLane = ({ acceptTask, children, color, icon, title, lighterColor }) => {
-  /**
-   * By setting a `.drag` class which says `pointer-events:none;`
-   * we prevent `dragEnter`, `dragLeave` and `dragOver` to be repeatedly fired many times
-   */
-  const [dragClass, setDragClass] = useState("");
-
-  return (
-    <div
-      className={classNames(styles.container, dragClass)}
-      onDragEnter={() => setDragClass(styles.drag)}
-      onDragLeave={() => setDragClass("")}
-      onDragOver={event => event.preventDefault()}
-      onDrop={event => {
-        event.preventDefault();
-        acceptTask(title);
-        setDragClass("");
-      }}
-      style={{ backgroundColor: color }}
-    >
-      <h2 className={styles.title} style={{ backgroundColor: lighterColor }}>
-        <span className={styles.titleLeft}>
-          <Icon icon={icon} iconSize={20} />
-          &nbsp; &nbsp;
-          {title}
-        </span>
-        <span>{children.length}</span>
-      </h2>
-      {children}
-    </div>
-  );
-};
+const TaskLane = ({ acceptTask, children, color, icon, lighterColor, title }) => (
+  <div
+    className={styles.container}
+    onDragOver={event => event.preventDefault()}
+    onDrop={event => {
+      event.preventDefault();
+      acceptTask(title);
+    }}
+    style={{ backgroundColor: color }}
+  >
+    <h2 className={styles.title} style={{ backgroundColor: lighterColor }}>
+      <span className={styles.titleLeft}>
+        <Icon icon={icon} iconSize={20} />
+        &nbsp; &nbsp;
+        {title}
+      </span>
+      <span>{children.length}</span>
+    </h2>
+    {children}
+  </div>
+);
 
 TaskLane.propTypes = {
   acceptTask: PropTypes.func.isRequired,
